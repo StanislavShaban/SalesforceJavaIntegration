057eb4e8349698fb6e25034e482a50304f224866
StanislavShaban <shabanstas@mail.ru>
2017-05-10 13:06:24 +0300

    src folder contains sample code with CRUD operations on Lead via Rest API

---
+++ src/main/java/connection/SalesforceConnection.java
@@ -0,0 +1,86 @@
+package connection;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+
+/**
+ * Created by jbUser on 5/10/2017.
+ */
+public class SalesforceConnection {
+
+    static final String USERNAME     = "stanislav@jetbi.com";
+    static final String PASSWORD     = "1210QeAd";
+    static final String LOGINURL     = "https://login.salesforce.com";
+    static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
+    static final String CLIENTID     = "3MVG9ZL0ppGP5UrDHVZHE9xOvyL6CDC.U7VqzX.Z7IK3l9GRqJrflrGK3lv70xlYQAlISMjWDYKbhctQy8fvS";
+    static final String CLIENTSECRET = "1903381799139045246";
+
+    public static void main(String[] args) {
+
+        HttpClient httpclient = HttpClientBuilder.create().build();
+
+        // Assemble the login request URL
+        String loginURL = LOGINURL +
+                GRANTSERVICE +
+                "&client_id=" + CLIENTID +
+                "&client_secret=" + CLIENTSECRET +
+                "&username=" + USERNAME +
+                "&password=" + PASSWORD;
+
+        // Login requests must be POSTs
+        HttpPost httpPost = new HttpPost(loginURL);
+        HttpResponse response = null;
+
+        try {
+            // Execute the login POST request
+            response = httpclient.execute(httpPost);
+        } catch (ClientProtocolException cpException) {
+            cpException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // verify response is HTTP OK
+        final int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.out.println("Error authenticating to Force.com: "+statusCode);
+            // Error is in EntityUtils.toString(response.getEntity())
+            return;
+        }
+
+        String getResult = null;
+        try {
+            getResult = EntityUtils.toString(response.getEntity());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        JSONObject jsonObject = null;
+        String loginAccessToken = null;
+        String loginInstanceUrl = null;
+        try {
+            jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
+            loginAccessToken = jsonObject.getString("access_token");
+            loginInstanceUrl = jsonObject.getString("instance_url");
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+        System.out.println(response.getStatusLine());
+        System.out.println("Successful login");
+        System.out.println("  instance URL: "+loginInstanceUrl);
+        System.out.println("  access token/session ID: "+loginAccessToken);
+
+        // release connection
+        httpPost.releaseConnection();
+    }
+
+}
